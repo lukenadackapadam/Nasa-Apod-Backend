ef4bb12b8d78cf521bbf4d14062ee2ae592505fd
luke <luke.nadackapadam@gmail.com>
2023-08-29 00:50:57 -0500

    Add show action and route

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,7 @@
 class PhotosController < ApplicationController
+  def show
+    response = HTTP.get("https://api.nasa.gov/planetary/apod?api_key=#{ENV["NASA_API_KEY"]}")
+    data = JSON.parse(response.body)
+    render json: data
+  end
 end
