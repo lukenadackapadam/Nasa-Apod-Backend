e26e5c20cf11029325a86078e50ccb48c0e9cd55
luke <luke.nadackapadam@gmail.com>
2023-09-02 13:02:43 -0500

    Add jbuilder, and user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+    )
+    if @user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/apod" => "photos#show"
+
+  post "/user" => "users#create"
 end
