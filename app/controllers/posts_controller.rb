a70fd4a11561edc354cc73a869f0733ca8c54cb7
luke <luke.nadackapadam@gmail.com>
2023-10-23 19:11:24 -0500

    Add update action for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,25 @@
 class PostsController < ApplicationController
   def create
     @post = Post.create(
-      user_id: params[:user_id],
-      comment: params[:comment],
-      upvote: params[:upvote],
+      user_id: current_user.id,
+      comment: params[:comment]
     )
     if @post.save
       render json: { message: "Post created successfully" }, status: :created
     else
       render json: { errors: @post.errors.full_messages }, status: :bad_request
+    end
+  end
+  
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.update(
+      comment: params[:comment] || @post.comment,
+    )
+    if @post.save
+      render json: { message: "Post updated successfully!" }, status: :created
+    else
+      render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/apod" => "photos#show"
 
   post "/post" => "posts#create"
+  patch "/post" => "posts#update"
 
   post "/user" => "users#create"
 end
