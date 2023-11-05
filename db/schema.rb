921f018cf9b4dfe68e8e691b383cbd998473b4e3
luke <luke.nadackapadam@gmail.com>
2023-11-05 15:31:35 -0600

    Add upvote model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_05_202926) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_05_211525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_05_202926) do
     t.datetime "updated_at", null: false
     t.integer "photo_id"
     t.text "post"
+  end
+
+  create_table "upvotes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
