3d3d8c096b98b6f9e7659ae01c1af857d3725e93
luke <luke.nadackapadam@gmail.com>
2023-12-20 11:21:39 -0600

    Edit photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,17 +7,17 @@ class PhotosController < ApplicationController
   def create
     response = HTTP.get("https://api.nasa.gov/planetary/apod?api_key=#{ENV["NASA_API_KEY"]}")
     data = JSON.parse(response.body)
-    
-    @photo = Photo.new(
+
+    photo = Photo.new(
       title: data["title"],
       explanation: data["explanation"],
       date: data["date"],
       hdurl: data["hdurl"],
     )
-    if @photo.save
-      render json: { message: "Photo saved successfully", photo: @photo }
+    if photo.save
+      render json: { message: "Photo saved successfully", photo: photo }
     else
-      render json: { error: "Failed to save photo", errors: @photo.errors.full_messages }
+      render json: { error: "Failed to save photo", errors: photo.errors.full_messages }
     end
   end
 
@@ -26,11 +26,14 @@ class PhotosController < ApplicationController
     local_date = DateTime.now
 
     utc_date = local_date.in_time_zone("UTC").beginning_of_day
-        
-    @photo = Photo.find_by(date: utc_date)
 
-    if @photo && @photo.date == utc_date
-      render :show
+    date_time_minus_one_millisecond = utc_date - Rational(1, 1000 * 60 * 60 * 24)
+    formatted_date_time = date_time_minus_one_millisecond.strftime('%Y-%m-%d 00:00:00 %Z')
+
+    photo = Photo.find_by(date: formatted_date_time)
+
+    if photo && photo.date == formatted_date_time
+      render json: photo
     else
       create()
     end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,33 +11,33 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(
-      user_id: params[:user_id],
+    post = Post.create(
+      user_id: current_user.id,
       photo_id: params[:photo_id],
       post: params[:post],
     )
-    if @post.save
+    if post.save
       render json: { message: "Post created successfully" }, status: :created
     else
-      render json: { errors: @post.errors.full_messages }, status: :bad_request
+      render json: { errors: post.errors.full_messages }, status: :bad_request
     end
   end
   
   def update
-    @post = Post.find_by(id: params[:id])
-    @post.update(
-      comment: params[:comment] || @post.comment,
+    post = Post.find_by(id: params[:id])
+    post.update(
+      comment: params[:comment] || post.comment,
     )
-    if @post.save
+    if post.save
       render json: { message: "Post updated successfully!" }, status: :created
     else
-      render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @post = Post.find_by(id: params[:id])
-    @post.destroy
+    post = Post.find_by(id: params[:id])
+    post.destroy
     render json: { message: "Post removed successfully!" }
   end
 end
