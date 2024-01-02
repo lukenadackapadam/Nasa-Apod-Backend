10638ecd5bf8c1b1658d582ee7ee9a522762bf26
luke <luke.nadackapadam@gmail.com>
2024-01-02 12:25:37 -0600

    Edit photos controller to search off title

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -22,17 +22,13 @@ class PhotosController < ApplicationController
   end
 
   def show
-    # Create a DateTime object in user's local timezone
-    local_date = DateTime.now
+    response = HTTP.get("https://api.nasa.gov/planetary/apod?api_key=#{ENV["NASA_API_KEY"]}")
+    data = JSON.parse(response.body)
+    title = data["title"]
 
-    utc_date = local_date.in_time_zone("UTC").beginning_of_day
-
-    date_time_minus_one_millisecond = utc_date - Rational(1, 1000 * 60 * 60 * 24)
-    formatted_date_time = date_time_minus_one_millisecond.strftime('%Y-%m-%d 00:00:00 %Z')
-
-    photo = Photo.find_by(date: formatted_date_time)
-
-    if photo && photo.date == formatted_date_time
+    photo = Photo.find_by(title: title)
+    
+    if photo != nil
       render json: photo
     else
       create()
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # Photo.create(id: 1, title: "A Martian Dust Devil Spins By", explanation: "It moved across the surface of Mars -- what was it? A dust devil. Such spinning columns of rising air are heated by the warm surface and are also common in warm and dry areas on planet Earth.  Typically lasting only a few minutes, dust devils become visible as they pick up loose red-colored dust, leaving the darker and heavier sand beneath intact.  Dust devils not only look cool -- they can leave visible trails, and have been credited with unexpected cleanings of the surfaces of solar panels. The images in the featured AI-interpolated video were captured in early August by the Perseverance rover currently searching for signs of ancient life in Jezero Crater. The six-second time-lapse video encapsulates a real duration of just over one minute.  Visible in the distance, the spinning dust devil was estimated to be passing by at about 20 kilometers per hour and extend up about 2 kilometers high.   Your Sky Surprise: What picture did APOD feature on your birthday? (post 1995)", date: "2023-11-07", hdurl: "https://www.youtube.com/embed/xB3QIEkNJgs?rel=0")
 
 
-User.create([
-  # { first_name: "Carl", last_name: "Sagan", email: "carl@example.com", password: "password" },
-  # { first_name: "Stephen", last_name: "Hawking", email: "stephen@example.com", password: "password" },
-  { first_name: "Luke", last_name: "N", email: "luke@example.com", password: "password" },
-])
+# User.create([
+#   { first_name: "Carl", last_name: "Sagan", email: "carl@example.com", password: "password" },
+#   { first_name: "Stephen", last_name: "Hawking", email: "stephen@example.com", password: "password" },
+#   { first_name: "Luke", last_name: "N", email: "luke@example.com", password: "password" },
+# ])
 
 # Post.create([
 #   { user_id: 1, photo_id: 1, post: "This amazing!! Man, I love space!!!" },
