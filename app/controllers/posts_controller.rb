791d5d4a4576d61d828440088eca4a142a282a09
luke <luke.nadackapadam@gmail.com>
2024-01-23 13:45:00 -0600

    Edit posts controller to check if user can post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,17 +6,22 @@ class PostsController < ApplicationController
 
   def posts_by_photo
     posts = Post.where(photo_id: params[:photo_id])
-    render json: posts
-    pp(posts)
+    user_can_post = !posts.any? { |post| post.user_id == current_user.id}  
+    posts_data = {posts: posts, user_can_post: user_can_post}
+    pp posts_data 
+    render json: posts_data
   end
 
   def create
+    # TODO: Verify user hasn't posted. 
+    puts params
     post = Post.create(
       user_id: current_user.id,
-      photo_id: params[:photo_id],
       post: params[:post],
+      photo_id: params[:photo_id],
     )
     if post.save
+      # posts_by_photo to stop re-render 
       render json: { message: "Post created successfully" }, status: :created
     else
       render json: { errors: post.errors.full_messages }, status: :bad_request
